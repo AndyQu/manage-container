afd6bd0798ca749c40cb0e317d3ce3d0575eee9f
曲国铖 <quguocheng@meituan.com>
2015-10-16 16:02:33 +0800

    remove docker folder

--- src/main/groovy/com/sankuai/srq/docker/ClearOutofDateDocker.groovy
+++ src/main/groovy/com/sankuai/srq/docker/ClearOutofDateDocker.groovy
@@ -8,41 +8,51 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class ClearOutofDateDocker {
-	def static Logger log = LoggerFactory.getLogger("ClearOutofDateDocker");
+	def static Logger logger = LoggerFactory.getLogger("ClearOutofDateDocker");
 	def static void main(String[] args){
 		
 		DockerClient dClient = new DockerClientImpl(dockerHost:'http://172.27.2.94:4243',)
 		DockerResponse response = dClient.ps(query:[all: true, size: true])
-		log.info "containers to be checked:${response.content.size()}"
+		logger.info "containers to be checked:${response.content.size()}"
 		if(response.status.success==true){
 			response.content.each { container->
 				long nowT=System.currentTimeMillis()/1000
 				if(nowT-container.Created>=60*60*8){
 					//12个小时以前启动的docker实例
-					log.info "[TODO][stop and remove container]${container.Names},${container.Status}"
-					log.info "stop container......"
+					logger.info "[TODO][stop and remove container]${container.Names},${container.Status}"
+					logger.info "stop container......"
 					response = dClient.stop(container.Id)
-					log.info response.status.success.toString()
+					logger.info response.status.success.toString()
 					if(response.status.success==true || response.status.code==304){
 						//304:资源未更改. 表示container已经被停止了
-						log.info "remove contaner......"
+						logger.info "remove contaner......"
 						response = dClient.rm(container.Id)
-						log.info response.status.success.toString()
+						logger.info response.status.success.toString()
 						if(response.status.success==true){
-							log.info "[success][remove container]${container.Names},${container.Status}"
+							logger.info "[success][remove container]${container.Names},${container.Status}"
 						}else{
-							log.info response.toString()
+							logger.error response.toString()
 						}
+                        container.Names.each {
+                            name->
+                                def result = new File("/docker-deploy/${name}").deleteDir
+                                if(result==true){
+                                    logger.info "removed folder:/docker-deploy/${name}"
+                                }else{
+                                    logger.error "failed removed folder:/docker-deploy/${name}"
+                                }
+                        }
+                        
 					}else{
-						log.info response.toString()
+						logger.error response.toString()
 					}
 				}else{
-					log.info "[Don't stop this container]${container.Names},${container.Status}"
+					logger.info "[Don't stop this container]${container.Names},${container.Status}"
 				}
 			}
 		}else{
-			log.info "[fail]docker ps -a"
-			log.info response.toString()
+			logger.info "[fail]docker ps -a"
+			logger.info response.toString()
 		}
 	}
 }
